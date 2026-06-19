63f4aeb4787497b6e25515af9031e9c8ee3ded47
Madhanbabu-1908 <89144800+Madhanbabu-1908@users.noreply.github.com>
2026-06-19 12:21:45 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,7 +4,10 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const DB_PATH   = path.join(__dirname, 'hackathon.db');
+const DB_PATH = path.join(
+  process.pkg ? path.dirname(process.execPath) : __dirname,
+  'hackathon.db'
+);
 
 let db = null;
 
